a84a97c483ec374ab9ccc74f0776f982ef760482
congtuyen295 <contuyen295@gmail.com>
2022-04-17 15:25:09 +0700

    cart ui

--- client/src/pages/cart/CartItem.js
+++ client/src/pages/cart/CartItem.js
@@ -1,134 +1,120 @@
-import { Link, useHistory } from "react-router-dom";
-import { Tooltip } from "antd";
-const formatter = new Intl.NumberFormat("vn");
-export default function CartItems({
-  dataCart,
-  setVisible,
-  token,
-  actionDeleteCart,
-  actionUpdateCartProduct,
-}) {
-  // const
-  const history = useHistory();
-  let totalSumCart = 0;
-  // function
-  const deleteCart = (index) => {
-    actionDeleteCart(index);
-  };
-  const onUpdateQuantity = (index, quantity) => {
-    const dataCart = {
-      index: index,
-      quantity: quantity,
-    };
-    if (quantity > 0) {
-      actionUpdateCartProduct(dataCart);
-    }
-  };
-  const totalSum = (cart) => {
-    return cart.product.price * cart.quantity;
-  };
-  const checkToken = () => {
-    if (token) {
-      setVisible(true);
-    } else {
-      history.push("/login");
-    }
-  };
-  const showTotalAmount = (cart) => {
-    if (cart.length > 0) {
-      for (let index = 0; index < cart.length; index++) {
-        totalSumCart += cart[index].product.price * cart[index].quantity;
-      }
-    }
-    return totalSumCart;
-  };
+import React from "react";
+import Breadcrumb from "../BreadCrumb";
+import thumb from "assets/images/giay1.jpg";
+import "./style.scss";
+
+const Cart = () => {
   return (
-    dataCart.length > 0 && (
-      <div className="group-card-item">
-        <div className="frames-card-item">
-          {dataCart.map((card, index) => (
-            <div className="card-items" key={index}>
-              <button
-                className="delete-item"
-                onClick={() => {
-                  deleteCart(index);
-                }}
-              >
-                <Tooltip placement="right" title="Xóa sản phẩm">
-                  <i className="fa fa-trash-o" />
-                </Tooltip>
-              </button>
-              <div className="card-image">
-                <img
-                  src={card.product.poster}
-                  alt={card.product._id}
-                  title="Xem chi tiết"
-                />
-              </div>
-              <div className="card-name">
-                <Link
-                  title="Xem chi tiết"
-                  to={`/detail-products?id_product=${card.product._id}&key=${card.product.key}`}
-                >
-                  <p>
-                    {card.product.name} - <span>size {card.product.size}</span>
-                  </p>
-                </Link>
-              </div>
-              <div className="card-rice">
-                <span>Giá</span>
-                <p>
-                  {formatter.format(card.product.price)} <u>đ</u>
-                </p>
-              </div>
-              <div className="card-quantity">
-                <span>Số lượng</span>
-                <div className="quantity-number">
-                  <button
-                    className="click-left"
-                    onClick={() => {
-                      onUpdateQuantity(index, card.quantity - 1);
-                    }}
-                  >
-                    -
-                  </button>
-                  <div>{card.quantity}</div>
-                  <button
-                    className="click-right"
-                    onClick={() => {
-                      onUpdateQuantity(index, card.quantity + 1);
-                    }}
-                    onClick={() =>
-                      card.quantity < 5
-                        ? onUpdateQuantity(index, card.quantity + 1)
-                        : card.quantity
-                    }
-                  >
-                    +
-                  </button>
+    <>
+      {/* <div className="grid wide pt-2 pb-2 mb-4 border-bottom">
+        <Breadcrumb />
+      </div> */}
+      <section className="main-cart-page main-container">
+        <div className="grid wide">
+          <div className="row">
+            <div className="col c-12 m-12 l-12">
+              <div className="header-cart">
+                <h1 className="title">Giỏ hàng</h1>
+                <div className="cart-page">
+                  <form>
+                    <div className="b-scroll">
+                      <div className="cart-thead">
+                        <div style={{ width: "17%" }}>Ảnh sản phẩm</div>
+                        <div style={{ width: "33%" }}>
+                          <span className="nobr">Tên sản phẩm</span>
+                        </div>
+                        <div style={{ width: "15%" }} className="a-center">
+                          <span className="nobr">Đơn giá</span>
+                        </div>
+                        <div style={{ width: "14%" }} className="a-center">
+                          Số lượng
+                        </div>
+                        <div style={{ width: "15%" }} className="a-center">
+                          Thành tiền
+                        </div>
+                        <div style={{ width: "6%" }}>Xoá</div>
+                      </div>
+                      <div className="cart-tbody">
+                        <div className="item-cart">
+                          <div style={{ width: "17%" }}>
+                            <a className="product-image" href="/">
+                              <img src={thumb} alt="" />
+                            </a>
+                          </div>
+                          <div className="a-center" style={{ width: "33%" }}>
+                            <h2 className="product-name" href="/">
+                              <a className="text2line">
+                                Giày Converse Chuck Taylor Festival Golden Mind
+                                Hi
+                              </a>
+                            </h2>
+                          </div>
+                          <div style={{ width: "15%" }} class="a-center">
+                            <span class="item-price">
+                              <span class="price">1.900.000₫</span>
+                            </span>
+                          </div>
+                          <div style={{ width: "14%" }}>
+                            <span className="input_qty_pr">
+                              <button type="button">-</button>
+                              <input type="text" value={1} />
+                              <button type="button">+</button>
+                            </span>
+                          </div>
+                          <div style={{ width: "15%" }} class="a-center">
+                            <span class="item-price">
+                              <span class="price">1.900.000₫</span>
+                            </span>
+                          </div>
+                          <div style={{ width: "6%" }}>
+                            <a class="remove-item-cart" title="Xóa" href="/">
+                              <span>
+                                <i class="fa-solid fa-trash-can"></i>
+                              </span>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <table class="shopping-cart-table-total mb-0">
+                        <tfoot>
+                          <tr>
+                            <td colspan="20" class="a-right">
+                              <span>Tổng tiền:</span>
+                            </td>
+                            <td class="a-right">
+                              <strong>
+                                <span class="totals_price price">
+                                  1.900.000₫
+                                </span>
+                              </strong>
+                            </td>
+                          </tr>
+                        </tfoot>
+                      </table>
+                      <ul class="checkout">
+                        <li>
+                          <button
+                            class="btn-proceed-checkout"
+                            title="Thực hiện thanh toán"
+                            type="button"
+                          >
+                            <span>Thực hiện thanh toán</span>
+                          </button>
+                          <button title="Tiếp tục mua hàng" type="button">
+                            <span>Tiếp tục mua hàng</span>
+                          </button>
+                        </li>
+                      </ul>
+                    </div>
+                  </form>
                 </div>
               </div>
-              <p className="total-sum">
-                Tổng cộng: {formatter.format(totalSum(card))} <u>đ</u>{" "}
-              </p>
             </div>
-          ))}
-        </div>
-        <div className="card-total-money">
-          <div className="total-money">
-            <h3>Thành tiền</h3>
-            <div className="group-total-money">
-              <p>
-                {formatter.format(showTotalAmount(dataCart))} <u>đ</u>
-              </p>
-              <span>(Đã bao gồm VAT nếu có)</span>
-            </div>
-            <button className="check-out" onClick={checkToken}>
-              Tiến hành đặt hàng
-            </button>
           </div>
         </div>
-      </div>
-    )
+      </section>
+    </>
   );
-}
+};
+
+export default Cart;
